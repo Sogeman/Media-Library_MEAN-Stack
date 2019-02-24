cf333aa4b4d7a721b1140b1313a574da00506a91
Rene Mandler <pw17l006@technikum-wien.at>
2019-02-24 22:39:28 +0100

    changes for heroku deploy (if it even works)

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ mongoose.connect(config.DB, {useNewUrlParser: true})
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api', mediaRoute)
+app.use('/api', mediaRoute);
+const distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 let port = process.env.PORT || 4000;
 
