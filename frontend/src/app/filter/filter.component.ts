a3a297b0c6eed6f973d24dd3b341c9746de13bdd
Rene Mandler <pw17l006@technikum-wien.at>
2019-03-29 10:14:09 +0100

    filter dropdown via ngfor and reset on route change

--- frontend/src/app/filter/filter.component.ts
+++ frontend/src/app/filter/filter.component.ts
@@ -10,11 +10,19 @@ import { FilterService } from '../filter.service';
 export class FilterComponent implements OnInit {
 
   @Output() location = new EventEmitter<string>();
+  selectedOption: string;
+  options = [
+    { name: 'Alles (alphab.)', value: '' },
+    { name: 'Bücher', value: 'Buch' },
+    { name: 'Filme', value: 'Film' },
+    { name: 'Spiele', value: 'Spiel' }
+  ];
 
   constructor(private router: Router, private filter: FilterService) {
     this.router.events.subscribe((event: Event) => { // subs to router event and emits the location
       if (event instanceof NavigationEnd) {
         this.location.emit(event.url);
+        this.selectedOption = 'default'; // reset filter box on route change
       }
     });
   }
@@ -24,6 +32,7 @@ export class FilterComponent implements OnInit {
 
   saveValue(value: string) {
     this.filter.setFilter(value); // sets value in filter service
+
   }
 
 }
